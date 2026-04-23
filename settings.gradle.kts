53e5540ced715b3c75ed28a21e8832be7b8d5927
lloydmeta <lloydmeta@gmail.com>
2026-04-23 11:06:30 +0900

    Allow bumps by renovatebot
    Signed-off-by: lloydmeta <lloydmeta@gmail.com>

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,8 +6,5 @@ pluginManagement {
         gradlePluginPortal()
         mavenLocal()
     }
-    plugins {
-        id(quarkusPluginId) version quarkusPluginVersion
-    }
 }
 rootProject.name="quarkus-tls-config-bug"
